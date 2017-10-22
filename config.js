872533c836abd12d555a196d626c591a863f27dc
Alberto Bastos <albertobastos@gmail.com>
2017-10-22 11:20:00 +0200

    Remove new gitignore files

--- config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-    //token: '461077328:AAEWQfe7xkQUTnsuJ_UiMpgeZN25YCwYOPo', // AbastosDevBot
-    //webhook: 'https://3sm5xuwf7j.execute-api.eu-west-1.amazonaws.com/dev/message' //  dev
-    token: '465184009:AAG-u82Jm_wIeaPx-bLmkZDEI4TEGceLkv0', // SuperPepinoBot
-    webhook: 'https://dq523kczr5.execute-api.eu-west-1.amazonaws.com/prod/message' // prod
-};
