3cd487e47c91cb7c01b3373beaed9d130b8caac2
Alberto Bastos <abastos@fundaciobit.org>
2017-10-25 07:41:47 +0200

    script for saving local files into another folder (like Dropbox...)

--- config.sample.js
+++ config.sample.js
@@ -2,6 +2,7 @@
 
 module.exports = {
     ratio: .25, // only reply on 25% of matches
+    localBackupPath: '<PATH_TO_YOUR_LOCAL_BACKUP_FOLDER>',
     token: '<YOUR_TELEGRAM_BOT_TOKEN>',
     webHook: '<YOUR_API_GATEWAY_URL>'
 };
